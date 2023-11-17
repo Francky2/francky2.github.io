b3d8081eca42c03cc85771b6973837d160e99991
Francky2 <96764755+Francky2@users.noreply.github.com>
2023-11-17 15:32:27 +0100

    Update script.js

--- Resource/script.js
+++ Resource/script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Concepteur de site web", " Rédacteur web", "Designer","Freelancer","Reférenceur SEO"],
+        strings: ["Concepteur de site web", "Concepteur de site web","Concepteur de site d'application mobile","Reférenceur SEO"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Concepteur de site web", "Concepteur d'application mobile"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
